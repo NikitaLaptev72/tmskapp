5ba69e3f10f26e61b9704369d95452e58aa79c0e
nekit <dfsd@zxcz>
2020-07-29 21:36:46 +0500

    First commit

--- routes/materials.routes.js
+++ routes/materials.routes.js
@@ -23,6 +23,37 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+
+
+router.put('/updateMaterial/:id', async (req, res) => {
+    try {
+        await Material.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({ message: "Материал обновлён!" })
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+});
+
+router.post("/addMaterial", async (req, res) => {
+    try {
+        const newMaterial = new Material(req.body)
+        await newMaterial.save()
+        res.status(201).json({ message: "Материал добавлен!"})
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+});
+
+router.delete("/deleteMaterial/:id", async (req, res) => {
+    try {
+        await Material.findByIdAndRemove({ _id: req.params.id })
+        res.status(200).json({ message: "Материал удалён!" })
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+    }
+
+});
+
 // router.put("/updateMaterial/:id", async (req, res) => {
 //     try {
 //         const material = await Material.findById({ _id: req.params.id });
@@ -38,40 +69,5 @@ router.get("/:id", async (req, res) => {
 //         res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
 //     }
 // })
-
-router.put('/updateMaterial/:id', async (req, res) => {
-    try {
-        await Material.findByIdAndUpdate(req.params.id, req.body)
-        res.status(200).json({ message: "Модератор обновлен!" })
-    } catch (e) {
-        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-    }
-});
-
-//Добавление и удаление заданий
-router.post("/addMaterial", async (req, res) => {
-    try {
-        const newMaterial = new Material(req.body)
-        await newMaterial.save()
-        res.status(201).json({ message: "Материал добавлен!", resultCode: 0 })
-    } catch (e) {
-        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-    }
-});
-// router.delete("/deleteTask/:id", async (req, res) => {
-//     try {
-//         await Task.findByIdAndRemove({ _id: req.params.id })
-//         res.status(200).json({ message: "Задание удалено!" })
-//     } catch (e) {
-//         res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-//     }
-
-// });
-
-
-
-
-
-
 
 module.exports = router
